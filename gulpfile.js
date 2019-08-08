59e2ad4551a08ac0607498ccf6df16efed7020ce
Thiago Murtinho <thiago.murtinho@live.com>
2019-08-08 09:28:30 -0300

    Task watch configure

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 const gulp = require('gulp')
+const { src, dest, parallel } = require('gulp');
+const pug = require('gulp-pug')
 const sass = require('gulp-ruby-sass')
 const watch = require('gulp-watch')
 
 
+gulp.task('pug', () => {
+    return src('pug/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('src'))
+})
+
+
 gulp.task('sass', () => {
     return sass('sass/**/*.sass')
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('src/css'))
 })
 
 
 gulp.task('watch', () => {
     gulp.watch('sass/**/*.sass', gulp.series('sass'))
+    gulp.watch('pug/*.pug', gulp.series('pug'))
 })
 
 
@@ -18,3 +28,4 @@ gulp.task('default', (done) => {
     console.log('Hello Gulp')
     done()
 })
+
