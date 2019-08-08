429773685c8a327e287a107beb6d43cd6b3e74d5
Thiago Murtinho <thiago.murtinho@live.com>
2019-08-08 01:59:17 -0300

    Create watch function

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp')
 const sass = require('gulp-ruby-sass')
+const watch = require('gulp-watch')
 
 
-
-gulp.task('sass', (done) => {
-    return sass('sass/*.sass')
+gulp.task('sass', () => {
+    return sass('sass/**/*.sass')
     .pipe(gulp.dest('css'))
+})
+
+
+gulp.task('watch', () => {
+    gulp.watch('sass/**/*.sass', gulp.series('sass'))
 })
 
 
