7f3487ca787b293d50f845002de74097d4bcb6c2
Thiago Murtinho <thiago.murtinho@live.com>
2019-08-09 23:08:34 -0300

    Fix: refatoring to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,24 @@
 const gulp = require('gulp')
-const { src, dest, parallel } = require('gulp');
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const watch = require('gulp-watch')
 
 
 gulp.task('pug', () => {
-    return src('pug/*.pug')
+    return gulp.src('pug/*.pug')
     .pipe(pug({ pretty: true }))
     .pipe(gulp.dest('src'))
 })
 
-
 gulp.task('sass', () => {
-    return sass('sass/**/*.sass')
+    return gulp.src('sass/**/*.sass')
+    .pipi(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('src/css'))
 })
-
-
 
 gulp.task('watch', () => {
     gulp.watch('sass/**/*.sass', gulp.series('sass'))
     gulp.watch('pug/*.pug', gulp.series('pug'))
 })
 
-
-gulp.task('default', (done) => {
-    console.log('Hello Gulp')
-    done()
-})
-
+gulp.task('default', gulp.series('pug', 'sass', 'watch'))
